7710bc93d5f94a6630873b112fab20c216ed9c58
Victor-Olufade <olufadevictor97@gmail>
2023-04-04 10:57:23 +0100

    create footer, hamburger menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+button.addEventListener('click', ()=>{
+    toogleMenu()
+})
+
+function toogleMenu () {
+    button.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
